627c72f78950c319ab09f68c74f84b9617bcd20e
alpha <453138391@qq.com>
2018-06-05 01:25:47 +0800

    newest

--- 6_AOPCoreAPI/src/com/alpha/DynamicProxy/AnotherTargetInterface.java
+++
@@ -1,8 +0,0 @@
-package com.alpha.DynamicProxy;
-
-/**
- * Created by Administrator on 2017/5/16.
- */
-public interface AnotherTargetInterface {
-    void  writeMessage(String message);
-}
--- 6_AOPCoreAPI/src/com/alpha/DynamicProxy/TargetInterface.java
+++
@@ -1,9 +0,0 @@
-package com.alpha.DynamicProxy;
-
-/**
- * Created by Administrator on 2017/5/16.
- */
-public interface TargetInterface {
-    void sayHello();
-    void sayBye();
-}
